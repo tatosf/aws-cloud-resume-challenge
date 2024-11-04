da868cb6e69b00fb70ad31186e74b8845fb24739
santiago-fischel-gutierrez-41ops <santiago.fischel.gutierrez@41ops.com>
2024-11-05 00:06:10 +0100

    [SFG] adding aws credentials to secrets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,20 +1,22 @@
+
+# Output the website endpoint
+output "website_url" {
+  value = aws_s3_bucket_website_configuration.website.website_endpoint
+  description = "URL of the S3 website bucket"
+}
 # Configure the AWS Provider
 provider "aws" {
-  region = "eu-west-1" 
+  region = "eu-west-1"
 }
 
-# Create or import existing s3 bucket
-resource "aws_s3_bucket" "website" {
+# Use data source instead of resource for existing bucket
+data "aws_s3_bucket" "website" {
   bucket = "personal-resume-website-tatofs"
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
-# enable website hosting
+# Website configuration
 resource "aws_s3_bucket_website_configuration" "website" {
-  bucket = aws_s3_bucket.website.id
+  bucket = data.aws_s3_bucket.website.id
 
   index_document {
     suffix = "index.html"
@@ -24,9 +26,9 @@ resource "aws_s3_bucket_website_configuration" "website" {
   }
 }
 
-#make bucket public
+# Public access block
 resource "aws_s3_bucket_public_access_block" "website" {
-  bucket = aws_s3_bucket.website.id
+  bucket = data.aws_s3_bucket.website.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -34,9 +36,9 @@ resource "aws_s3_bucket_public_access_block" "website" {
   restrict_public_buckets = false
 }
 
-# Add bucket policy for public read access
+# Bucket policy
 resource "aws_s3_bucket_policy" "website" {
-  bucket = aws_s3_bucket.website.id
+  bucket = data.aws_s3_bucket.website.id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -45,14 +47,8 @@ resource "aws_s3_bucket_policy" "website" {
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.website.arn}/*"
+        Resource  = "${data.aws_s3_bucket.website.arn}/*"
       }
     ]
   })
-}
-
-# Output the website endpoint
-output "website_url" {
-  value = aws_s3_bucket_website_configuration.website.website_endpoint
-  description = "URL of the S3 website bucket"
 }
