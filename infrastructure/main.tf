0146d41565ab7e302625d2c03a6f701bf3c416d8
santiago-fischel-gutierrez-41ops <santiago.fischel.gutierrez@41ops.com>
2024-11-05 00:22:18 +0100

    [SFG] adding aws credentials to secrets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,16 +1,20 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-west-1" 
 }
 
-# Use data source for existing bucket
-data "aws_s3_bucket" "website" {
+# create s3 bucket
+resource "aws_s3_bucket" "website" {
   bucket = "personal-resume-website-tatofs"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
-# Enable website hosting
+# enable website hosting
 resource "aws_s3_bucket_website_configuration" "website" {
-  bucket = data.aws_s3_bucket.website.id
+  bucket = aws_s3_bucket.website.id
 
   index_document {
     suffix = "index.html"
@@ -20,9 +24,9 @@ resource "aws_s3_bucket_website_configuration" "website" {
   }
 }
 
-# Make bucket public
+#make bucket public
 resource "aws_s3_bucket_public_access_block" "website" {
-  bucket = data.aws_s3_bucket.website.id
+  bucket = aws_s3_bucket.website.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -32,7 +36,7 @@ resource "aws_s3_bucket_public_access_block" "website" {
 
 # Add bucket policy for public read access
 resource "aws_s3_bucket_policy" "website" {
-  bucket = data.aws_s3_bucket.website.id
+  bucket = aws_s3_bucket.website.id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -41,13 +45,8 @@ resource "aws_s3_bucket_policy" "website" {
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
-        Resource  = "${data.aws_s3_bucket.website.arn}/*"
+        Resource  = "${aws_s3_bucket.website.arn}/*"
       }
     ]
   })
-}
-
-# Output the website URL
-output "website_url" {
-  value = data.aws_s3_bucket.website.website_endpoint
 }
