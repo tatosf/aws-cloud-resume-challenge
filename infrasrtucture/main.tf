1985d4ce7a40237c53bc9441689b9f3c03ff94b0
santiago-fischel-gutierrez-41ops <santiago.fischel.gutierrez@41ops.com>
2024-11-04 20:17:22 +0100

    [SFG] adding website and infra chunk 1

---
+++ infrasrtucture/main.tf
@@ -0,0 +1,49 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-1" 
+}
+
+# create s3 bucket
+resource "aws_s3_bucket" "website" {
+    bucket = "resume-website"
+}
+
+# enable website hosting
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+}
+
+#make bucket public
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Add bucket policy for public read access
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website.arn}/*"
+      },
+    ]
+  })
+}
+
