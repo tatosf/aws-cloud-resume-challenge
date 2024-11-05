fc375e8bcf0bc26cd02e966e3365a278668ae5cf
tatosf <santiagofischel1@gmail.com>
2024-11-05 23:37:54 +0100

    [SFG] pushing 53 route and CloudFront integration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,7 +9,6 @@ resource "aws_s3_bucket" "website" {
   bucket = "personal-resume-website-tatofs"
 }
 
-# Make bucket public FIRST
 resource "aws_s3_bucket_public_access_block" "website" {
   bucket = aws_s3_bucket.website.id
 
@@ -21,7 +20,6 @@ resource "aws_s3_bucket_public_access_block" "website" {
 
 resource "aws_s3_bucket_policy" "website" {
   depends_on = [aws_s3_bucket_public_access_block.website]
-  
   bucket = aws_s3_bucket.website.id
   policy = jsonencode({
     Version = "2012-10-17"
@@ -47,5 +45,62 @@ resource "aws_s3_bucket_website_configuration" "website" {
 
   error_document {
     key = "error.html"
+  }
+}
+
+# Create CloudFront distribution
+resource "aws_cloudfront_distribution" "website" {
+  origin {
+    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id   = "s3-website"
+  }
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "s3-website"
+    min_ttl                = 0
+    default_ttl            = 300
+    max_ttl                = 1200
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+# Create Route53 record
+resource "aws_route53_zone" "website" {
+  name = "santiagofischel.com" 
+}
+
+resource "aws_route53_record" "website" {
+  zone_id = aws_route53_zone.website.id
+  name    = "santiagofischel.com" 
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    evaluate_target_health = false
   }
 }
